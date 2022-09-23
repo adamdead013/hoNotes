fc4881b6e909875562aedacb16c6a9ae81871194
adamdead013 <placebo.games121@gmail.com>
2022-09-23 21:12:58 +0700

    Добавьте файлы проекта.

---
+++ MyNotes/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace MyNotes
+{
+    partial class mainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.textBoxNote = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.fileDirectory = new System.Windows.Forms.Label();
+            this.fileOpen = new System.Windows.Forms.Button();
+            this.fileopenWindow = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(0, 0);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(94, 29);
+            this.buttonCreate.TabIndex = 1;
+            this.buttonCreate.Text = "Создать";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // textBoxNote
+            // 
+            this.textBoxNote.Location = new System.Drawing.Point(0, 64);
+            this.textBoxNote.Multiline = true;
+            this.textBoxNote.Name = "textBoxNote";
+            this.textBoxNote.Size = new System.Drawing.Size(1262, 473);
+            this.textBoxNote.TabIndex = 2;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(100, 0);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(94, 29);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Сохранить";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // fileDirectory
+            // 
+            this.fileDirectory.AutoSize = true;
+            this.fileDirectory.Location = new System.Drawing.Point(12, 32);
+            this.fileDirectory.Name = "fileDirectory";
+            this.fileDirectory.Size = new System.Drawing.Size(50, 20);
+            this.fileDirectory.TabIndex = 4;
+            this.fileDirectory.Text = "label1";
+            // 
+            // fileOpen
+            // 
+            this.fileOpen.Location = new System.Drawing.Point(200, 0);
+            this.fileOpen.Name = "fileOpen";
+            this.fileOpen.Size = new System.Drawing.Size(94, 29);
+            this.fileOpen.TabIndex = 5;
+            this.fileOpen.Text = "Открыть";
+            this.fileOpen.UseVisualStyleBackColor = true;
+            this.fileOpen.Click += new System.EventHandler(this.fileOpen_Click);
+            // 
+            // fileopenWindow
+            // 
+            this.fileopenWindow.FileName = "openFileDialog1";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1262, 549);
+            this.Controls.Add(this.fileOpen);
+            this.Controls.Add(this.fileDirectory);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxNote);
+            this.Controls.Add(this.buttonCreate);
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(1280, 47);
+            this.Name = "mainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "hoNotes";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox textBoxNote;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label fileDirectory;
+        private System.Windows.Forms.Button fileOpen;
+        private System.Windows.Forms.OpenFileDialog fileopenWindow;
+    }
+}
---
+++ MyNotes/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.SymbolStore;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyNotes;
+
+namespace MyNotes
+{
+    public partial class mainForm : Form
+    {
+        #region Переменные
+        string path = @"Unnamed"; //переменная пути файла
+        string tag = ".txt"; //расширение сохраняемого файла
+        Random random = new(); //переменная? для имени файла, если файл Unnamed.txt уже существует в директории
+
+
+        #endregion
+
+
+        #region Запуск Form
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            textBoxNote.Hide();
+            fileDirectory.Hide();
+            textBoxNote.ScrollBars = ScrollBars.Both;
+        }
+        #endregion
+
+        /// <summary>
+        /// Создание файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            textBoxNote.Text = "";
+            if (File.Exists(path + tag)) //если файл Unnamed.txt существует
+            {
+                File.Create(path + random.Next(0, 10).ToString() + tag); //создаём файл с рандомным числом в названии
+            }
+            else //иначе
+            {
+
+                File.Create(path + tag); //создаём файл Unnamed.txt
+            }
+            textBoxNote.Show(); //показываем текстбокс
+            fileDirectory.Text = path + tag; //указываем в текст лейбла путь до файла
+            fileDirectory.Show();//показываем лейбл
+        }
+
+        /// <summary>
+        ///  Сохранение файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(fileDirectory.Text)) //если файл сущетсвует
+            {
+                File.AppendAllText(fileDirectory.Text, textBoxNote.Text); //записываем в него текст
+            }
+            else //если нет 
+            {
+                MessageBox.Show("Файл не существует"); //показываем уведомление
+            }
+        }
+        /// <summary>
+        /// Открытие файла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void fileOpen_Click(object sender, EventArgs e) 
+        {
+            fileopenWindow.ShowDialog(); //открываем окно в котором пользователь выбирает файл
+            fileDirectory.Show(); //показываем лейбл в котором хранится текст пути до файла
+            textBoxNote.Show(); //показываем текстбокс
+            textBoxNote.Text = File.ReadAllText(fileopenWindow.FileName); //записываем текст из файла в текстбокс
+            fileDirectory.Text = fileopenWindow.FileName; //передаем путь до файла в лейбл
+
+        }
+
+
+    }
+}
+
